4aa2cad7d3ef4d180acfc8b4828e2aeb454b3fd5
Ayman Yasser <72998532+Ayman409@users.noreply.github.com>
2023-05-03 13:11:49 +0300

    s

--- lib/src/anim_search_widget.dart
+++ lib/src/anim_search_widget.dart
@@ -346,48 +346,46 @@ class _AnimSearchBarState extends State<AnimSearchBar>
                 color: toggle == 0 ? widget.color : widget.textFieldColor,
 
                 borderRadius: BorderRadius.circular(17.0),
-                child: Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      IconButton(
-                        splashRadius: 19.0,
-                        padding: EdgeInsets.all(0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      splashRadius: 19.0,
+                      padding: EdgeInsets.all(0),
 
-                        ///if toggle is 1, which means it's open. so show the back icon, which will close it.
-                        ///if the toggle is 0, which means it's closed, so tapping on it will expand the widget.
-                        ///prefixIcon is of type Icon
-                        icon: widget.prefixIcon != null
-                            ? toggle == 1
-                                ? Icon(
-                                    Icons.arrow_back_ios,
-                                    color: widget.textFieldIconColor,
-                                  )
-                                : widget.prefixIcon!
-                            : Icon(
-                                toggle == 1
-                                    ? Icons.arrow_back_ios
-                                    : Icons.search,
-                                // search icon color when closed
-                                color: toggle == 0
-                                    ? widget.searchIconColor
-                                    : widget.textFieldIconColor,
-                                size: 20.0,
-                              ),
-                        onPressed: () {},
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
+                      ///if toggle is 1, which means it's open. so show the back icon, which will close it.
+                      ///if the toggle is 0, which means it's closed, so tapping on it will expand the widget.
+                      ///prefixIcon is of type Icon
+                      icon: widget.prefixIcon != null
+                          ? toggle == 1
+                              ? Icon(
+                                  Icons.arrow_back_ios,
+                                  color: widget.textFieldIconColor,
+                                )
+                              : widget.prefixIcon!
+                          : Icon(
+                              toggle == 1 ? Icons.arrow_back_ios : Icons.search,
+                              // search icon color when closed
+                              color: toggle == 0
+                                  ? widget.searchIconColor
+                                  : widget.textFieldIconColor,
+                              size: 20.0,
+                            ),
+                      onPressed: () {},
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Expanded(
+                      child: Text(
                         widget.label,
                         style: TextStyle(
                             fontSize: 12,
                             color: Color(0xff3A3F3D),
                             fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
