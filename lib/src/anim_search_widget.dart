a6383b087b6571a7a809ce2fdac5bf64bef565fe
Ayman Yasser <72998532+Ayman409@users.noreply.github.com>
2023-05-03 14:24:55 +0300

    s

--- lib/src/anim_search_widget.dart
+++ lib/src/anim_search_widget.dart
@@ -382,65 +382,62 @@ class _AnimSearchBarState extends State<AnimSearchBar>
                                   fontSize: 12,
                                 ),
                               )
-                            : IconButton(
-                                splashRadius: 19.0,
-                                padding: EdgeInsets.all(0),
-
-                                ///if toggle is 1, which means it's open. so show the back icon, which will close it.
-                                ///if the toggle is 0, which means it's closed, so tapping on it will expand the widget.
-                                ///prefixIcon is of type Icon
-                                icon: widget.prefixIcon != null
-                                    ? toggle == 1
-                                        ? Icon(
-                                            Icons.arrow_back_ios,
-                                            color: widget.textFieldIconColor,
-                                          )
-                                        : widget.prefixIcon!
-                                    : Icon(
-                                        toggle == 1
-                                            ? Icons.arrow_back_ios
-                                            : Icons.search,
-                                        // search icon color when closed
-                                        color: toggle == 0
-                                            ? widget.searchIconColor
-                                            : widget.textFieldIconColor,
-                                        size: 20.0,
-                                      ),
-                                onPressed: () {
-                                  setState(
-                                    () {
-                                      ///if the search bar is closed
-                                      if (toggle == 0) {
-                                        toggle = 1;
-                                        setState(() {
-                                          ///if the autoFocus is true, the keyboard will pop open, automatically
-                                          if (widget.autoFocus)
-                                            FocusScope.of(context)
-                                                .requestFocus(focusNode);
-                                        });
-
-                                        ///forward == expand
-                                        _con.forward();
-                                      } else {
-                                        ///if the search bar is expanded
-                                        toggle = 0;
-
-                                        ///if the autoFocus is true, the keyboard will close, automatically
-                                        setState(() {
-                                          if (widget.autoFocus)
-                                            unfocusKeyboard();
-                                        });
-
-                                        ///reverse == close
-                                        _con.reverse();
-                                      }
-                                    },
-                                  );
-                                },
-                              ),
+                            : Container()
                       ],
                     )
-                  : Container(),
+                  : IconButton(
+                      splashRadius: 19.0,
+                      padding: EdgeInsets.all(0),
+
+                      ///if toggle is 1, which means it's open. so show the back icon, which will close it.
+                      ///if the toggle is 0, which means it's closed, so tapping on it will expand the widget.
+                      ///prefixIcon is of type Icon
+                      icon: widget.prefixIcon != null
+                          ? toggle == 1
+                              ? Icon(
+                                  Icons.arrow_back_ios,
+                                  color: widget.textFieldIconColor,
+                                )
+                              : widget.prefixIcon!
+                          : Icon(
+                              toggle == 1 ? Icons.arrow_back_ios : Icons.search,
+                              // search icon color when closed
+                              color: toggle == 0
+                                  ? widget.searchIconColor
+                                  : widget.textFieldIconColor,
+                              size: 20.0,
+                            ),
+                      onPressed: () {
+                        setState(
+                          () {
+                            ///if the search bar is closed
+                            if (toggle == 0) {
+                              toggle = 1;
+                              setState(() {
+                                ///if the autoFocus is true, the keyboard will pop open, automatically
+                                if (widget.autoFocus)
+                                  FocusScope.of(context)
+                                      .requestFocus(focusNode);
+                              });
+
+                              ///forward == expand
+                              _con.forward();
+                            } else {
+                              ///if the search bar is expanded
+                              toggle = 0;
+
+                              ///if the autoFocus is true, the keyboard will close, automatically
+                              setState(() {
+                                if (widget.autoFocus) unfocusKeyboard();
+                              });
+
+                              ///reverse == close
+                              _con.reverse();
+                            }
+                          },
+                        );
+                      },
+                    ),
             ),
           ],
         ),
